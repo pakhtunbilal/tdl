6014a5eeff9a85e1376d7480796a3e4221a75ba2
pakhtunbilal <microbilalpthn@gmail.com>
2023-03-20 19:12:09 +0530

    commit1 of todolist-V2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyparser = require('body-parser')
-const date = require(__dirname + "/date.js")
+const _ = require('lodash')
+const mongoose = require('mongoose')
 
 
 const app = express();
@@ -9,35 +10,108 @@ app.use(bodyparser.urlencoded({ extended: true }))
 app.use(express.static(__dirname + "/public"))
 
 
-var items = ["Buy food", "Eat food"];
-var workitems = [];
+mongoose.connect('mongodb+srv://pakhtun_bilal:Ikidgaf6.9@cluster0.vhqvbk9.mongodb.net/todolistDB')
 
+const itemsSchema = new mongoose.Schema ( {
+    name : String
+})
 
-app.get('/', (req, res) => {
-    let day = date.getDate()
-    res.render("list", { listtitle: day, NewListItems: items })
+const Item = mongoose.model('Item',itemsSchema)
+
+const work1 = new Item({
+    name:"ek kaam karo",
+})
+const work2 = new Item({
+    name:"do kaam karo",
+})
+const work3 = new Item({
+    name:"teen kaam karo",
+})
+
+const defaultItems = [work1,work2,work3]
+
+const listschema ={
+    name : String,
+    items : [itemsSchema]
+}
+
+const List = mongoose.model('list',listschema) 
+
+app.get('/', async(req, res) => {
+    let day = 'Today'
+    let found = await Item.find({})
+    if(found.length == 0){
+    await   Item.insertMany(defaultItems)
+   }else{
+ 
+   }
+    res.render("list", { listtitle: day, NewListItems: found })
+});
+
+app.get('/:custom',async(req,res)=>{
+    const customlist = _.capitalize(req.params.custom)
+
+    let found = await List.findOne({name:customlist})
+    if(found){
+        res.render("list",{ listtitle:found.name, NewListItems:found.items})
+    }else{
+        const list = await new List({
+            name : customlist,
+            items: defaultItems
+        })
+        await list.save()
+        res.redirect('/'+customlist)
+    }
+    
 })
 
 
-app.post('/', (req, res) => {
-    let item = req.body.input;
-    let list = req.body.list
+app.post('/', async(req, res) => {
 
-    if (list === "work") {
-        workitems.push(item)
-        res.redirect("/work")
-    } else {
-        items.push(item)
-        res.redirect('/')
+    let InputName = req.body.input;
+    let listName = req.body.list
+    const Input ={
+        name :InputName
     }
 
+    if(listName === "Today"){
+      await  Item.insertMany(Input);
+    res.redirect('/')
+    }else{
+        let foundList = await List.findOne({name:listName})
+       await foundList.items.push(Input)
+       await foundList.save();
+        res.redirect('/' + listName)
+    }
+
+    
 
 });
 
+app.post('/delete', async(req,res)=>{
+   const CheckedId =req.body.checkbox;
+   const ListName = req.body.ListName;
 
-app.get('/work', (req, res) => {
-    res.render("list", { listtitle: "work", NewListItems: workitems })
+   if(ListName === "Today"){
+   await Item.deleteOne({_id :CheckedId})
+   res.redirect('/')
+   }else{
+   let found =  await List.findOneAndUpdate(
+        {name:ListName},
+        {$pull:{
+            items: {
+                _id : CheckedId
+            }
+        }}
+    )
+    res.redirect('/' + ListName)
+   }
+
+   
 })
+
+
+
 
 
 app.get('/about', (req, res) => {
